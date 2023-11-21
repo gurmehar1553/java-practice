2e161d946079dfd4c75a14e0d8b602732bbd29ab
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-11-21 10:02:24 +0530

    Updated Vowel

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -2,7 +2,7 @@ import java.util.*;
 class VowelConsonant{
      public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
-	char ch = sc.nextChar();
+	char ch = sc.next().charAt(0);
 	if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
 		System.out.println("Vowel");
 	}else{
